1b2599259baef3cece3a763681db48289770283c
gloria murillo <andreamurillo558@gmail.com>
2021-03-28 17:22:43 -0600

    Creacion de primeros formularios.

--- Ajustes.xaml.cs
+++ Ajustes.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace SistemaDental
 {
     /// <summary>
-    /// Lógica de interacción para MainWindow.xaml
+    /// Interaction logic for Ajustes.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Ajustes : Window
     {
-        public MainWindow()
+        public Ajustes()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Login login = new Login();
-            login.Show();
         }
     }
 }
