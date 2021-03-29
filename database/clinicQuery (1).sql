c72f6404b88993facb27022e47f48d082501f160
Javier Castro <acej2207@gmail.com>
2021-03-28 19:10:28 -0600

    Ingresando datos a la base de datos clinicaDental

--- database/clinicQuery (1).sql
+++ database/clinicQuery (1).sql
@@ -22,11 +22,8 @@ idpuesto int,
 genero varchar(20),
 contraseña varchar(20),
 estado bit
-<<<<<<< HEAD
 constraint pk_idempleado primary key(id_e),
-=======
 constraint pk_idempleado primary key(id_empleado),
->>>>>>> feature-database
 constraint fk_Empleado_Puesto foreign key(idpuesto) references Clinica.puesto(id_puesto) 
 )
 
@@ -63,11 +60,8 @@ id_material1 int,
 id_tratamiento1 int,
 cantidad int,
 constraint fk_idm foreign key(id_material1) references Clinica.inventario(id_material),
-<<<<<<< HEAD
 constraint fk_idt foreign key(id_t1) references Clinica.tratamiento(id_t)
-=======
 constraint fk_idt foreign key(id_tratamiento1) references Clinica.tratamiento(id_tratamiento)
->>>>>>> feature-database
 on update no action
 on delete no action
 )
@@ -89,11 +83,8 @@ tipo_transacción varchar(15),
 cantidad int,
 fecha date,
 dinero_disponible int,
-<<<<<<< HEAD
 constraint pk_idtrans primary key(id_trans)
-=======
 constraint pk_idtrans primary key(id_transaccion)
->>>>>>> feature-database
 )
 
 create table Clinica.citas(
@@ -101,13 +92,10 @@ id_empleado varchar(50),
 id_paciente varchar(50),
 fechaCita datetime,
 id_tratamiento int,
-<<<<<<< HEAD
 constraint fk_Empleado_Citas foreign key(id_empleado) references Clinica.empleado(id_e),
 constraint fk_Paciente_Citas foreign key(id_paciente) references Clinica.pacientes(id_p),
-=======
 constraint fk_Empleado_Citas foreign key(id_empleado) references Clinica.empleado(id_empleado),
 constraint fk_Paciente_Citas foreign key(id_paciente) references Clinica.pacientes(id_paciente),
->>>>>>> feature-database
 )
 
 --Constraints
@@ -147,20 +135,12 @@ GO
 
 Alter table Clinica.empleado WITH CHECK
 add constraint CHK_Clinica_Empleados$FormatoIdentidad
-<<<<<<< HEAD
-check ([id_e] LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-=======
 check ([id_empleado] LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
->>>>>>> feature-database
 GO
 
 Alter table Clinica.pacientes WITH CHECK
 add constraint CHK_Clinica_Pacientes$FormatoIdentidad
-<<<<<<< HEAD
-check ([id_p] LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
-=======
 check ([id_paciente] LIKE '[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]')
->>>>>>> feature-database
 GO
 
 alter table Clinica.empleado with check
@@ -238,11 +218,7 @@ go
 create proc	VerificarExistenciaEmpleado
 @user varchar(50)
 as 
-<<<<<<< HEAD
-select * from Clinica.empleado where id_e=@user
-=======
 select * from Clinica.empleado where id_empleado=@user
->>>>>>> feature-database
 go
 
 
@@ -262,11 +238,7 @@ create proc EditarEmpleados
 @contraseña varchar(20),
 @estado bit
 as
-<<<<<<< HEAD
-update Clinica.empleado set nombre=@nombre,apellido=@apellido,telefono=@telefono,correo=@correo,idpuesto=@puesto,genero=@genero,contraseña=@contraseña, estado=@estado where id_e=@id
-=======
 update Clinica.empleado set nombre=@nombre,apellido=@apellido,telefono=@telefono,correo=@correo,idpuesto=@puesto,genero=@genero,contraseña=@contraseña, estado=@estado where id_empleado=@id
->>>>>>> feature-database
 go
 
 
@@ -293,11 +265,7 @@ go
 create proc MostrarPacienteEspecifico
 @id varchar(50)
 as
-<<<<<<< HEAD
-select *from Clinica.pacientes where id_p=@id
-=======
 select *from Clinica.pacientes where id_paciente=@id
->>>>>>> feature-database
 go
 
 
@@ -309,11 +277,7 @@ create proc EditarPacientes
 @edad int,
 @genero varchar(20)
 as
-<<<<<<< HEAD
-update Clinica.pacientes set nombre=@nombre,apellido=@apellido,telefono=@telefono,edad=@edad,genero=@genero where id_p=@id
-=======
 update Clinica.pacientes set nombre=@nombre,apellido=@apellido,telefono=@telefono,edad=@edad,genero=@genero where id_paciente=@id
->>>>>>> feature-database
 go
 
 
@@ -344,11 +308,7 @@ create proc EditarTratamiento
 @precio int,
 @id int
 as
-<<<<<<< HEAD
-update Clinica.tratamiento set nombre=@nombre,precio=@precio where id_t=@id
-=======
 update Clinica.tratamiento set nombre=@nombre,precio=@precio where id_tratamiento=@id
->>>>>>> feature-database
 go
 
 -----TABLA INVENTARIO
@@ -401,11 +361,7 @@ go
 create proc MostrarHistorial
 @idpaciente varchar(50)
 as
-<<<<<<< HEAD
-select id_paciente,tratamiento.nombre,fecha from Clinica.historial inner join Clinica.tratamiento on historial.id_t2=tratamiento.id_t where @idpac=id_paciente
-=======
 select id_paciente,tratamiento.nombre,fecha from Clinica.historial inner join Clinica.tratamiento on historial.id_t2=tratamiento.id_tratamiento where @idpaciente=id_paciente
->>>>>>> feature-database
 go
  
 
@@ -415,49 +371,19 @@ create proc EditarHistorial
 @idtratamiento int,
 @id int
 as
-<<<<<<< HEAD
-update Clinica.historial set id_paciente=@idpaciente, id_t2=@idtratamiento, fecha=@fecha where id_h=@id
-=======
 update Clinica.historial set id_paciente=@idpaciente, id_t2=@idtratamiento, fecha=@fecha where id_historial=@id
->>>>>>> feature-database
 go
 
 create proc MostrarUso
 @id int
 as
-<<<<<<< HEAD
-select tratamiento.nombre,tratamiento.precio,tratamiento_inventario.cantidad from Clinica.tratamiento_inventario inner join Clinica.tratamiento on tratamiento_inventario.id_t1=tratamiento.id_t inner join Clinica.inventario on tratamiento_inventario.id_material1=inventario.id_material where @id=id_material;
-=======
 select tratamiento.nombre,tratamiento.precio,tratamiento_inventario.cantidad from Clinica.tratamiento_inventario inner join Clinica.tratamiento on tratamiento_inventario.id_tratamiento1=tratamiento.id_tratamiento inner join Clinica.inventario on tratamiento_inventario.id_material1=inventario.id_material where @id=id_material;
->>>>>>> feature-database
 go
 
 
 create proc materialnecesario
 @id bigint
 as
-<<<<<<< HEAD
-select inventario.nombre, tratamiento_inventario.cantidad from Clinica.tratamiento_inventario inner join Clinica.tratamiento on tratamiento_inventario.id_t1=tratamiento.id_t inner join Clinica.inventario on tratamiento_inventario.id_material1=inventario.id_material where @id=tratamiento.id_t
-go
-
-create proc actCantidad
-@cantidad int,
-@material varchar(50)
-as
-update Clinica.inventario set cantidad=cantidad-@cantidad where nombre=@material
-go
-
-/*Tabla Citas*/
-create proc IngresoCitas
-@idempleado varchar(50),
-@idpaciente varchar(50),
-@fecha datetime,
-@idtratamiento int
-as
-insert into Clinica.citas values (@idempleado,@idpaciente,@fecha,@idtratamiento)
-go
-
-=======
 select inventario.nombre, tratamiento_inventario.cantidad from Clinica.tratamiento_inventario inner join Clinica.tratamiento on tratamiento_inventario.id_tratamiento1=tratamiento.id_tratamiento inner join Clinica.inventario on tratamiento_inventario.id_material1=inventario.id_material where @id=tratamiento.id_tratamiento
 go
 
@@ -478,7 +404,27 @@ as
 insert into Clinica.citas values (@idempleado,@idpaciente,@fecha,@idtratamiento)
 go
 
->>>>>>> feature-database
+
+
+
+create proc actCantidad
+@cantidad int,
+@material varchar(50)
+as
+update Clinica.inventario set cantidad=cantidad-@cantidad where nombre=@material
+go
+
+/*Tabla Citas*/
+create proc IngresoCitas
+@idempleado varchar(50),
+@idpaciente varchar(50),
+@fecha datetime,
+@idtratamiento int
+as
+insert into Clinica.citas values (@idempleado,@idpaciente,@fecha,@idtratamiento)
+go
+
+
 create proc EditarCitas
 @idempleado varchar(50),
 @idpaciente varchar(50),
@@ -514,52 +460,6 @@ insert into Clinica.tratamiento values('Extracción',300)
 insert into Clinica.tratamiento values('Ortodoncia',1000)
 
 /*Ingreso de material*/
-
-<<<<<<< HEAD
-insert into inventario values('Recinas',100)
-insert into inventario values('Gazas',80)
-insert into inventario values('Anestecia',50)
-insert into inventario values('Agujas',100)
-insert into inventario values('Brackets',300)
-insert into inventario values('Alambre',100)
-insert into inventario values('Ules',500)
-insert into inventario values('adhesivo',40)
-insert into inventario values('Acido',50)
-
-select * from inventario 
-select * from tratamiento
-
-/*Ingreso de relacion tratamiento y material
-delete from tratamiento_inventario where id_material1 = 3
-select * from tratamiento_inventario
-insert into tratamiento_inventario values(1,1,2)
-insert into tratamiento_inventario values(2,2,3)
-insert into tratamiento_inventario values(4,3,1)
-insert into tratamiento_inventario values(5,3,28)
-insert into tratamiento_inventario values(6,3,2)
-insert into tratamiento_inventario values(7,3,28)
-insert into tratamiento_inventario values(8,3,4)
-insert into tratamiento_inventario values(9,3,1)
-insert into tratamiento_inventario values(3,3,1)
-insert into tratamiento_inventario values(3,2,1)
-insert into tratamiento_inventario values(3,1,1)
-insert into tratamiento_inventario values(4,2,1)
-insert into tratamiento_inventario values(4,1,1)
-<<<<<<< HEAD
-
-*/
-
-
-
-
-
-
-
-
-=======
-*/
->>>>>>> feature-database
-=======
 insert into Clinica.inventario values('Recinas',100),('Gazas',80),('Anestecia',50)
 ,('Agujas',100)
 ,('Brackets',300)
@@ -570,8 +470,9 @@ insert into Clinica.inventario values('Recinas',100),('Gazas',80),('Anestecia',5
 
 
 
---Ingreso de relacion tratamiento y material
 
+
+--Ingreso de relacion tratamiento y material
 
 insert into Clinica.tratamiento_inventario values(1,1,2)
 ,(2,2,3)
@@ -586,4 +487,4 @@ insert into Clinica.tratamiento_inventario values(1,1,2)
 ,(3,1,1)
 ,(4,2,1)
 ,(4,1,1)
->>>>>>> feature-database
+
