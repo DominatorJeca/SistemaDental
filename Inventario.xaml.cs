d833ad8e3d66b471cc14de87710015d802e5739f
Javier Castro <acej2207@gmail.com>
2021-04-02 15:49:07 -0600

    Se muestra el inventario y los tratamientos según el material seleccionado

--- Inventario.xaml.cs
+++ Inventario.xaml.cs
@@ -11,6 +11,16 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.Sql;
+using System.Media;
+using System.Drawing;
+using System.Windows.Forms;
+using System.CodeDom.Compiler;
+using MessageBox = System.Windows.MessageBox;
+using DataGridCell = System.Windows.Forms.DataGridCell;
 
 namespace SistemaDental
 {
@@ -19,9 +29,28 @@ namespace SistemaDental
     /// </summary>
     public partial class Inventario : Window
     {
+        ClaseInventario inventario = new ClaseInventario();
+        int num = 1;
         public Inventario()
         {
             InitializeComponent();
+            MostrarMaterial();
+
+        }
+
+        public void MostrarMaterial()
+        {
+            dgv_Materiales.ItemsSource = inventario.MostrarInventario();
+            dgv_Materiales.SelectedValuePath = "IdMaterial";
+        }
+
+       
+
+   
+        private void dgv_Materiales_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            dgv_Tratamiento.ItemsSource = inventario.mostrarUsoTratamiento(Convert.ToInt32(dgv_Materiales.SelectedValue)).DefaultView;
         }
     }
 }
+
