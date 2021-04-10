ebb4f6c3c6f8c9e3cf1fb4ad6fddeaf93f854ca7
Nallely Reyes <na_reyesd@unicah.edu>
2021-04-09 23:36:55 -0600

    Funcion verificar valores,LimpiarFormulario y btnAgregarUsuario

--- AgregarUsuario.xaml.cs
+++ AgregarUsuario.xaml.cs
@@ -33,7 +33,7 @@ namespace SistemaDental
 
         public void ObtenerValores()
         {
-            usuario.Id=Convert.ToString(txtAgregarIdentidad.Text);
+            usuario.Id = Convert.ToString(txtAgregarIdentidad.Text);
             usuario.Nombre = Convert.ToString(txtAgregarNombre.Text);
             usuario.Apellido = Convert.ToString(txtAgregarApellido.Text);
             usuario.Telefono = Convert.ToString(txtAgregarTelefono.Text);
@@ -45,10 +45,82 @@ namespace SistemaDental
             usuario.Administrador = false;
         }
 
+        private bool VerificarValores()
+        {
+            if (txtAgregarApellido.Text == string.Empty || txtAgregarNombre.Text == string.Empty || txtAgregarCorreo.Text == string.Empty
+                || txtAgregarIdentidad.Text == string.Empty || txtAgregarTelefono.Text == string.Empty || txtAgregarContra.Text == string.Empty)
+            {
+                MessageBox.Show("Por favor ingresa todos los valores en las cajas de texto");
+                return false;
+            }
+            else if (cmbSexo.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el Sexo del nuevo empleado");
+                return false;
+            }
+            else if (cmbPuesto.SelectedValue == null)
+            {
+                MessageBox.Show("Por favor selecciona el puesto del nuevo empleado");
+                return false;
+            }
+            else if (txtConfirmarContra != txtAgregarContra)
+            {
+                MessageBox.Show("La confirmaciónn de contraseña no coincide");
+                return false;
+            }
+            else if (txtConfirmarCorreo != txtAgregarContra)
+            {
+                MessageBox.Show("La confirmaciónn de correo no coincide");
+                return false;
+            }
+
+
+            return true;
+        }
+
         private void btnAgregarUsuario_Click(object sender, RoutedEventArgs e)
         {
-            ObtenerValores();
-      
+            // Verificar que se ingresaron los valores requeridos
+            if (VerificarValores())
+            {
+                try
+                {
+                    // Obtener los valores para el empleado
+                    ObtenerValores();
+
+                    // Insertar los datos del usuario 
+                    usuario.IngresarUsuario(usuario);
+
+                    // Mensaje de inserción exitosa
+                    MessageBox.Show("¡Datos insertados correctamente!");
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Ha ocurrido un error al momento de insertar el empleado...");
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    LimpiarFormulario();
+                }
+            }
+
+        }
+
+        private void LimpiarFormulario()
+        {
+            txtAgregarApellido.Text = string.Empty;
+            txtAgregarNombre.Text = string.Empty;
+            txtAgregarCorreo.Text = string.Empty;
+            txtAgregarIdentidad.Text = string.Empty; 
+            txtAgregarTelefono.Text= string.Empty;
+            txtAgregarContra.Text = string.Empty;
+            txtConfirmarContra.Text = string.Empty;
+            txtConfirmarCorreo.Text = string.Empty;
+            cmbPuesto.SelectedValue = null;
+            cmbSexo.SelectedValue = null;
+
         }
     }
 }
