e78c2eb876349561042a1e91240401bb83a8dc31
JeffreyMencias <lenardjeffrey.menciashernandez@gmail.com>
2021-04-14 23:43:43 -0600

    Se empezó a crear una lista donde se mostrarán todos los id de los pacientes para poder seleccionarlos en un combo box.

--- ClasePaciente.cs
+++ ClasePaciente.cs
@@ -14,5 +14,48 @@ namespace SistemaDental
 {
     class ClasePaciente
     {
+        private static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SistemaDental.Properties.Settings.ClinicaBDConnection"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+
+
+        public ClasePaciente()
+        {
+
+        }
+
+        public elPaciente(int id, string nombrePaciente) {
+            Id = id;
+            NombrePaciente = nombrePaciente;
+        }
+
+        public List<elPaciente> MostrarPacientes()
+        {
+            //Inicializar una lista vacia de pacientes
+            List<elPaciente> paciente = new List<elPaciente>();
+
+            try
+            {
+                sqlConnection.Open();
+
+                //Crear el comando SQL
+                SqlCommand sqlCommand = new SqlCommand("Mostrarpacientes", sqlConnection);
+
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+
+                //Obtener los datos de los pacientes
+                using (SqlDataReader rdr = sqlCommand.ExecuteReader())
+                {
+                    while (())
+                        paciente.Add(new ClasePaciente(Id = Convert.ToInt32(rdr["id_paciente"]), NombrePaciente = rdr["nombre"].ToString()));
+
+                }
+
+                return paciente;
+            }
+
+        }
+
+
     }
 }
