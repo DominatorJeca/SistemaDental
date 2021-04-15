310f51ba46ac1ff881fef7bdcbc40e369db17492
JeffreyMencias <lenardjeffrey.menciashernandez@gmail.com>
2021-04-14 22:48:20 -0600

    Creacion de la clase ClasePaciente.

---
+++ ClasePaciente.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//Agregar Namespaces Necesarios para Conexion SQL
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace SistemaDental
+{
+    class ClasePaciente
+    {
+    }
+}
