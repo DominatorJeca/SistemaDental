6ca6eb867cd90e04e074c5dbbe0cb5b83a64a53e
andreamurillo17 <andreamurillo558@gmail.com>
2021-04-14 23:41:49 -0600

    Creacion de Clase Caja y Edicion de form Caja.

--- Caja.xaml.cs
+++ Caja.xaml.cs
@@ -12,6 +12,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+// Agregar los namespaces necesarios para conectarse a SQL
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
 namespace SistemaDental
 {
     /// <summary>
@@ -19,9 +25,51 @@ namespace SistemaDental
     /// </summary>
     public partial class Caja : Window
     {
+
+        ClaseCaja caja = new ClaseCaja();
+
+        private SqlConnection sqlConnection;
         public Caja()
         {
             InitializeComponent();
+            MostrarCaja();
+            IngresarDatos();
+        }
+
+        public void MostrarCaja()
+        {
+            dgvCaja.ItemsSource = caja.MostrarCaja();
+            dgvCaja.SelectedValuePath = "id_transaccion";
+        }
+
+        public void IngresarDatos()
+        {
+            dgvCaja.ItemsSource = caja.IngresarDatos();
+            dgvCaja.SelectedValuePath = "id_transaccion";
+        }
+
+        private void btnRealizar_Click(object sender, RoutedEventArgs e)
+        {
+            if (Convert.ToInt32(dgvCaja.SelectedValue) > 0)
+            {
+                try
+                {
+                    ObtenerValues();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+        private void ObtenerValues()
+        {
+            caja.cantidad = Convert.ToInt32(txtCantidadCaja.Text);
+            caja.dinero_disponible = Convert.ToInt32(txtDineroCaja.Text);
+            
         }
     }
 }
---
+++ ClaseCaja.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Agregar los namespaces necesarios para conectarse a SQL
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace SistemaDental
+{
+    class ClaseCaja
+    {
+
+        private static string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["SistemaDental.Properties.Settings.ClinicaBDConnection"].ConnectionString;
+        private SqlConnection sqlConnection = new SqlConnection(connectionString);
+
+        public int id_transaccion { get; set; }
+
+        public string tipo_transacción { get; set; }
+
+        public int cantidad { get; set; }
+
+        public int fecha { get; set; }
+
+        public double dinero_disponible { get; set; }
+
+
+        public ClaseCaja()
+        {
+
+        }
+
+        public List<ClaseCaja> MostrarCaja()
+        {
+            sqlConnection.Open();
+
+            try
+            {
+                SqlCommand command = new SqlCommand("MostrarTransaccion", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<ClaseCaja> TestList = new List<ClaseCaja>();
+                ClaseCaja test = null;
+
+                while (reader.Read())
+                {
+                    test = new ClaseCaja();
+                    test.id_transaccion = int.Parse(reader["id_transaccion"].ToString());
+                    test.tipo_transacción = reader["MostrarTransaccion"].ToString();
+                    test.cantidad = int.Parse(reader["cantidad"].ToString());
+                    TestList.Add(test);
+                }
+
+                return TestList;
+            }
+            catch
+            {
+
+                throw;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+        public List<ClaseCaja> IngresarDatos()
+        {
+            sqlConnection.Open();
+
+            try
+            {
+                SqlCommand command = new SqlCommand("IngresoTransaccion", sqlConnection);
+                command.CommandType = CommandType.StoredProcedure;
+                SqlDataReader reader = command.ExecuteReader();
+
+                List<ClaseCaja> TestList = new List<ClaseCaja>();
+                ClaseCaja test = null;
+
+                while (reader.Read())
+                {
+                    test = new ClaseCaja();
+                    test.id_transaccion = int.Parse(reader["id_transaccion"].ToString());
+                    test.tipo_transacción = reader["MostrarTransaccion"].ToString();
+                    test.cantidad = int.Parse(reader["cantidad"].ToString());
+                    TestList.Add(test);
+                }
+
+                return TestList;
+            }
+
+            
+            catch
+            {
+                throw;
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+
+    }
+
+}
+    
+
+
