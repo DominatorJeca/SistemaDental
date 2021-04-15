fbbf76c3486e359f1271a855bdb60e423e3462e4
JeffreyMencias <lenardjeffrey.menciashernandez@gmail.com>
2021-04-15 15:22:17 -0600

    Se configuro el boton cancelar, deshace todos los cambios y limpia pantalla. Se configuro la funcion de habilitar o deshabilitar ciertos elementos del formulario.

--- VerPaciente.xaml.cs
+++ VerPaciente.xaml.cs
@@ -20,19 +20,31 @@ namespace SistemaDental
     public partial class VerPaciente : Window
     {
 
+        //Creacion de un objeto de tipo ClasePaciente
         private ClasePaciente unPaciente = new ClasePaciente();
+
+        /// <summary>
+        /// Componentes que se inicializan junto al form
+        /// </summary>
         public VerPaciente()
         {
             InitializeComponent();
             MostrarPacientes();
+            HabilitacionDeshabilitacion(false, true);
         }
 
+        /// <summary>
+        /// Boton para regresar al menu anterior
+        /// </summary>
         private void btnRegresar_Click(object sender, RoutedEventArgs e)
         {
             Pacientes pacientes = new Pacientes();
             pacientes.Show();
         }
 
+        /// <summary>
+        /// Funcion para llenar el combobox con los id extraidos de la BD mediante la clase ClasePaciente
+        /// </summary>
         public void MostrarPacientes()
         {
             cmbPaciente.ItemsSource = unPaciente.MostrarPacientes();
@@ -40,18 +52,64 @@ namespace SistemaDental
             cmbPaciente.DisplayMemberPath = "id_paciente";
         }
 
+        /// <summary>
+        /// Funcion para habilitar o deshabilitar los botones, textbox y combobox del formulario
+        /// </summary>
+        public void HabilitacionDeshabilitacion(bool habilitacionGrupoA, bool habilitacionGrupoB)
+        {
+            btnGuardarPaciente.IsEnabled = habilitacionGrupoA;
+            btnCancelar.IsEnabled = habilitacionGrupoA;
+            txtNombre.IsEnabled = habilitacionGrupoA;
+            txtApellido.IsEnabled = habilitacionGrupoA;
+            txtTelefono.IsEnabled = habilitacionGrupoA;
+            txtIdentidad.IsEnabled = habilitacionGrupoA;
+            cmbSexo.IsEnabled = habilitacionGrupoA;
+
+            btnEditarPaciente.IsEnabled = habilitacionGrupoB;
+            cmbPaciente.IsEnabled = habilitacionGrupoB;
+        }
+
+        /// <summary>
+        /// Funcion para limpiar el contenido del Form
+        /// </summary>
+        public void LimpiarPantalla ()
+        {
+            txtNombre.Text = null;
+            txtApellido.Text = null;
+            txtIdentidad.Text = null;
+            txtTelefono.Text = null;
+            cmbSexo.SelectedItem = null;
+            cmbPaciente.SelectedItem = null;
+            dtgHistorial = null;
+        }
+
+        /// <summary>
+        /// Boton para editar el paciente seleccionado en el combobox de Id Paciente
+        /// </summary>
         private void btnEditarPaciente_Click(object sender, RoutedEventArgs e)
         {
             try
             {
+                //Mensaje de advertencia si no selecciona ningun elemento
                 if (cmbPaciente.SelectedItem == null)
                 {
                     MessageBox.Show("Por favor seleccione un paciente.");
+                }
+                else {
+                    //Deshabilita el combobox paciente y el boton editar
+                    //Habilita los demás botones y textbox para poder editar los datos
+                    HabilitacionDeshabilitacion(true, false);
                 }
             
             }
             catch
             { }
+        }
+
+        private void btnCancelar_Click(object sender, RoutedEventArgs e)
+        {
+            HabilitacionDeshabilitacion(false, true);
+            LimpiarPantalla();
         }
     }
 }
