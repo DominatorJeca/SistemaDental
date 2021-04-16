45dfe138f676a717462542b254e6e066096a3353
andreamurillo17 <andreamurillo558@gmail.com>
2021-04-15 19:01:00 -0600

    Modificacion de Caja.xaml.cs y ClaseCaja

--- Caja.xaml.cs
+++ Caja.xaml.cs
@@ -26,51 +26,93 @@ namespace SistemaDental
     public partial class Caja : Window
     {
 
-
-        ClaseCaja caja = new ClaseCaja();
+        
+        private ClaseCaja caja = new ClaseCaja();
 
         private SqlConnection sqlConnection;
         public Caja()
         {
             InitializeComponent();
             MostrarCaja();
-            IngresarDatos();
+            
         }
 
         public void MostrarCaja()
         {
             dgvCaja.ItemsSource = caja.MostrarCaja();
             dgvCaja.SelectedValuePath = "id_transaccion";
-        }
-
-        public void IngresarDatos()
-        {
-            dgvCaja.ItemsSource = caja.IngresarDatos();
-            dgvCaja.SelectedValuePath = "id_transaccion";
-        }
-
-        private void btnRealizar_Click(object sender, RoutedEventArgs e)
-        {
-            if (Convert.ToInt32(dgvCaja.SelectedValue) > 0)
-            {
-                try
-                {
-                    ObtenerValues();
-
-                }
-                catch (Exception)
-                {
-
-                    throw;
-                }
-            }
+            dgvCaja.DisplayMemberPath = "tipo_transacción";
         }
 
         private void ObtenerValues()
         {
-            caja.cantidad = Convert.ToInt32(txtCantidadCaja.Text);
-            caja.dinero_disponible = Convert.ToInt32(txtDineroCaja.Text);
             
+            caja.Cantidad = Convert.ToInt32(txtCantidadCaja.Text);
+            caja.Dinero_disponible = Convert.ToInt32(txtDineroCaja.Text);
+
+        }
+
+
+        /// <summary>
+        /// Verifica que todos los valores no esten vacios
+        /// </summary>
+        
+            private bool VerificarValores()
+            {
+                if(txtCantidadCaja.Text == string.Empty || txtDineroCaja.Text == string.Empty)
+                {
+                    MessageBox.Show("Por favor ingrese todos los valores en las cajas de texto");
+                    return false;
+                } 
+                else if(rbIngreso.IsChecked == false)
+                {
+                    MessageBox.Show("Por favor escoga una opcion de transaccion.");
+                    return false;
+                }   
+                else if(rbEgreso.IsChecked == false)
+                {
+                    MessageBox.Show("Por favor escoga una opcion de transaccion.");
+                    return false;
+                }
+
+            return true;
+            }
+
+
+
+        public void LimpiarForm()
+        {
+            txtCantidadCaja.Text = string.Empty;
+            txtDineroCaja.Text = string.Empty;
+            rbEgreso.IsChecked = false;
+            rbIngreso.Content = true;
+        }
+        private void btnRealizar_Click(object sender, RoutedEventArgs e)
+        {
+            // Verificar los datos que fueron ingresados
+            if(VerificarValores() == true)
+            {
+
+                try
+                {
+                    //Obtener los valores de caja
+                    ObtenerValues();
+
+                    //Insertar datos de Caja
+                    caja.IngresarCaja(caja);
+
+                    // Mensaje de insercion exitosa a la base
+                    MessageBox.Show("Datos insertaos correctamente");
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Ha occurido un error al momento de insertar el empleado");
+                    Console.WriteLine(ex.Message);
+                }
+                
+            }
+
         }
     }
 }
