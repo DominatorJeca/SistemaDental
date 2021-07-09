8d83b8cfbf5d2d1a2a484dfe0519e7a4e5683722
FranciscoMadrid <ffms-a@outlook.com>
2021-07-09 17:29:25 -0600

    *

--- Procedimientos.cs
+++ Procedimientos.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace SistemaDental
 {
     class Procedimientos
     {
+      
     }
 }
