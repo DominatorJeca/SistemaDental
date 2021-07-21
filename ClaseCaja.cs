ddfee0788436770599dd02731505987bc2e434fc
TyAzur <maykin500ella@gmail.com>
2021-07-20 20:22:37 -0600

    citas modificado

--- ClaseCaja.cs
+++ ClaseCaja.cs
@@ -15,7 +15,7 @@ namespace SistemaDental
     {
 
         //Variable Miembro
-        private static string connectionString = @"server = .; Initial Catalog = clinicaDental; Integrated Security = True; MultipleActiveResultSets=true"; 
+        private static string connectionString = @"server = DESKTOP-8MP5H5G; Initial Catalog = clinicaDental; Integrated Security = True; MultipleActiveResultSets=true"; 
         private SqlConnection sqlConnection = new SqlConnection(connectionString);
 
         // Propiedades
