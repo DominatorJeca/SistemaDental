dd68a6dd88ecd668cdf9e19c5c136506546ecc34
FranciscoMadrid <ffms-a@outlook.com>
2021-08-01 02:06:27 -0600

    *Implementacion de website en linea

--- MVCCV/Vista/MenuInicioVista.xaml.cs
+++ MVCCV/Vista/MenuInicioVista.xaml.cs
@@ -42,8 +42,8 @@ namespace SistemaDental.MVCCV.Vista
 
         private void btnHelp_Click(object sender, RoutedEventArgs e)
         {
-            string url = "http://localhost/ProjectoClinicaDental/vistas/";
-            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            string url = "http://sistemadentalhn.epizy.com/ProjectoClinicaDental/vistas/index.php";
+            System.Diagnostics.Process.Start(url);
         }
 
         private void btnTratamientos_Click(object sender, RoutedEventArgs e)
